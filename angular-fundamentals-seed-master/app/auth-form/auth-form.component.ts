fcc57f487b8c9a24145341ff415eeb20185363a7
Artur <artur.rozwalak@gmail.com>
2023-12-04 21:43:22 +0000

    Using ElementRef and nativeElement

--- angular-fundamentals-seed-master/app/auth-form/auth-form.component.ts
+++ angular-fundamentals-seed-master/app/auth-form/auth-form.component.ts
@@ -1,6 +1,8 @@
 import {
   Component,
   Output,
+  ElementRef,
+  ViewChild,
   ViewChildren,
   AfterViewInit,
   EventEmitter,
@@ -17,25 +19,26 @@ import { User } from "./auth-form.interface";
 
 @Component({
   selector: "auth-form",
+  styles: [
+    `
+      .email {
+        border-color: #9f72e6;
+      }
+    `,
+  ],
   template: `
     <div>
       <form (ngSubmit)="onSubmit(form.value)" #form="ngForm">
         <ng-content select="h3"></ng-content>
         <label>
           Email address
-          <input type="email" name="email" ngModel />
+          <input type="email" name="email" ngModel #email />
         </label>
         <label>
           Password
           <input type="password" name="password" ngModel />
         </label>
         <ng-content select="auth-remember"></ng-content>
-        <auth-message
-          [style.display]="showMessage ? 'inherit' : 'none'"
-        ></auth-message>
-        <auth-message
-          [style.display]="showMessage ? 'inherit' : 'none'"
-        ></auth-message>
         <auth-message
           [style.display]="showMessage ? 'inherit' : 'none'"
         ></auth-message>
@@ -53,11 +56,21 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
   @ViewChildren(AuthMessageComponent)
   message: QueryList<AuthMessageComponent>;
 
+  @ViewChild("email") email: ElementRef;
+
   constructor(private cd: ChangeDetectorRef) {}
 
   showMessage: boolean;
 
   ngAfterViewInit(): void {
+    this.email.nativeElement.setAttribute(
+      "placeholder",
+      "Enter your email address"
+    );
+
+    this.email.nativeElement.classList.add("email");
+    this.email.nativeElement.focus();
+
     if (this.message) {
       this.message.forEach((message) => {
         message.days = 30;
