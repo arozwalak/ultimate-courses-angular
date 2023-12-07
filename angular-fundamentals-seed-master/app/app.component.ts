cb18bc2d8e986b5c137e57a0449c42aeed7c1ed2
Artur Rozwalak <artur.rozwalak@landbay.co.uk>
2023-12-07 15:43:29 +0000

    Dynamic components

--- angular-fundamentals-seed-master/app/app.component.ts
+++ angular-fundamentals-seed-master/app/app.component.ts
@@ -2,6 +2,8 @@ import {
   AfterContentInit,
   Component,
   ComponentFactoryResolver,
+  ComponentRef,
+  TemplateRef,
   ViewChild,
   ViewContainerRef,
 } from "@angular/core";
@@ -12,14 +14,28 @@ import { AuthFormComponent } from "./auth-form/auth-form.component";
 
 @Component({
   selector: "app-root",
+  styles: [
+    `
+      :host > div {
+        display: block;
+      }
+    `,
+  ],
   template: `
     <div>
+      <button (click)="destroyComponent()">Destroy</button>
+      <button (click)="moveComponent()">Move</button>
       <div #entry></div>
+      <template #tmpl let-name let-location="location">
+        {{ name }} : {{ location }}
+      </template>
     </div>
   `,
 })
 export class AppComponent implements AfterContentInit {
   @ViewChild("entry", { read: ViewContainerRef }) entry: ViewContainerRef;
+  @ViewChild("tmpl") tmpl: TemplateRef<any>;
+  component: ComponentRef<AuthFormComponent>;
 
   constructor(private resolver: ComponentFactoryResolver) {}
 
@@ -27,7 +43,23 @@ export class AppComponent implements AfterContentInit {
     const authFormFactory =
       this.resolver.resolveComponentFactory(AuthFormComponent);
 
-    const component = this.entry.createComponent(authFormFactory);
+    this.entry.createComponent(authFormFactory);
+    this.component = this.entry.createComponent(authFormFactory, 0);
+    this.component.instance.title = "Create account";
+    this.component.instance.submitted.subscribe(this.loginUser);
+
+    this.entry.createEmbeddedView(this.tmpl, {
+      $implicit: "Todd Motto",
+      location: "England, UK",
+    });
+  }
+
+  destroyComponent() {
+    this.component.destroy();
+  }
+
+  moveComponent() {
+    this.entry.move(this.component.hostView, 1);
   }
 
   loginUser(user: User) {
